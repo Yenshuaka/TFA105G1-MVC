fe6f789bfcf897eec3c8f6d07c064be805689573
Gin <shuaka60@gmail.com>
2022-02-17 00:54:34 +0800

    update 2/17 00:54

---
+++ src/main/java/com/product/product/controller/ProductDisplayController.java
@@ -0,0 +1,75 @@
+package com.product.product.controller;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.product.product.model.ProductBean;
+import com.product.product.model.ProductService;
+
+@Controller
+public class ProductDisplayController {
+
+	@Autowired
+	private ProductService productService;
+	
+	@RequestMapping("/ProductDisplayController")
+	public String displayAll(HttpSession session, String page, Model model) {
+
+	
+		List<ProductBean> list = productService.select(null);
+		if (page == null) {
+			page = "1";
+		}
+		int pageindex = Integer.valueOf(page);
+		List<ProductBean> list2 = new ArrayList();
+
+		for (int i = (pageindex - 1) * 4; i <= ((pageindex * 4) - 1); i++) {
+			if (list.get(i) != null) {
+				list2.add(list.get(i));
+			}
+		}
+
+		List imgids = new ArrayList();
+		Connection connection;
+		try {
+			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/TFA105G1?serverTimezone=Asia/Taipei",
+					"root", "password");
+
+			for (int i = 0; i < list2.size(); i++) {
+				if (list2.get(i) != null) {
+					PreparedStatement ps = connection
+							.prepareStatement("SELECT * FROM PRODUCT_IMG where PRODUCT_ID = ? limit 1");
+					ps.setInt(1, list2.get(i).getProductid());
+					ResultSet rSet = ps.executeQuery();
+
+					while (rSet.next()) {
+						imgids.add(rSet.getInt(1));
+					}
+				}
+			}
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		model.addAttribute("page", page);
+		model.addAttribute("list2", list2);
+		model.addAttribute("imgids", imgids);
+		return "product-display";
+
+	}
+	
+}
