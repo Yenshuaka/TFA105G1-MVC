93dc8cedcf4323249d069b0cf201cf6d0e058114
Nick <nexnick000@gmail.com>
2022-02-26 20:54:34 +0800

    welcome index admin

---
+++ src/main/java/com/admin/model/AdminDAO.java
@@ -0,0 +1,32 @@
+package com.admin.model;
+
+import java.util.List;
+
+public class AdminDAO implements AdminDAOInterface {
+
+	@Override
+	public AdminVO select(Integer empno) {
+		return null;
+	}
+
+	@Override
+	public List<AdminVO> select() {
+		return null;
+	}
+
+	@Override
+	public AdminVO insert(AdminVO admin) {
+		return null;
+	}
+
+	@Override
+	public AdminVO update(AdminVO admin) {
+		return null;
+	}
+
+	@Override
+	public boolean delete(Integer empno) {
+		return false;
+	}
+
+}
