daed9b31ddd9d837fe4ba43900659cbdcbfb2f68
Gin <shuaka60@gmail.com>
2022-03-02 02:12:27 +0800

    3/2 product update

--- src/main/java/com/product/product/controller/ProductDisplayController.java
+++ src/main/java/com/product/product/controller/ProductDisplayController.java
@@ -13,6 +13,7 @@ import java.text.DateFormat;
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import javax.persistence.PersistenceContext;
@@ -79,7 +80,7 @@ public class ProductDisplayController {
 	
 	@RequestMapping("/ProductDisplayController")
 	public String displayAll(HttpSession session, String page, Model model, String keyword,
-			String price, String type, String[] city) {
+			String price, String type, String[] city, String[] time) {
 		
 		session.removeAttribute("keyword");
 		session.removeAttribute("type");
@@ -174,8 +175,35 @@ public class ProductDisplayController {
 			}	
 		}
 		
-
-		session.setAttribute("list", list);
+		
+		if(time!=null && time.length!=0) {
+			int total = list.size();
+			List<ProductBean> newlist = new ArrayList<ProductBean>();
+			for(int i = 0; i < total ; i++) {
+				if(Arrays.asList(time).contains("a") && list.get(i).getTraveltime()<4) {
+					newlist.add(list.get(i));
+				}
+				if(Arrays.asList(time).contains("b") && list.get(i).getTraveltime()>=4 && list.get(i).getTraveltime()<24) {
+					newlist.add(list.get(i));
+				}
+				if(Arrays.asList(time).contains("c") && list.get(i).getTraveltime()>=24 && list.get(i).getTraveltime()<48) {
+					newlist.add(list.get(i));
+				}
+				if(Arrays.asList(time).contains("d") && list.get(i).getTraveltime()>=48) {
+					newlist.add(list.get(i));
+				}
+			}
+			
+			session.setAttribute("list", newlist);
+			
+		}else {
+			session.setAttribute("list", list);
+		}
+		
+		
+		
+		session.setAttribute("totalproduct", list.size());
+//		session.setAttribute("list", list);
 		return "redirect:/MVC/PageHandler";
 		
 
@@ -215,6 +243,9 @@ public class ProductDisplayController {
 		
 //		找出此頁該顯示的圖片們
 		List imgids = new ArrayList();
+		List<Integer> commentcount = new ArrayList<Integer>();
+		List<Double> avg = new ArrayList<Double>();
+		List<String> cities = new ArrayList<String>();
 		Connection connection;
 		try {
 			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/TFA105G1?serverTimezone=Asia/Taipei",
@@ -230,16 +261,77 @@ public class ProductDisplayController {
 					while (rSet.next()) {
 						imgids.add(rSet.getInt(1));
 					}
+					
+					//以下找出有幾則評論
+					ps = connection
+							.prepareStatement("SELECT COUNT(*) FROM PRODUCT_COMMENT WHERE PRODUCT_ID = ?");
+					ps.setInt(1, list2.get(i).getProductid());
+					rSet = ps.executeQuery();
+
+					while (rSet.next()) {
+						commentcount.add(rSet.getInt(1));
+					}
+					
+					//以下找出平均分數
+					
+					ps = connection
+							.prepareStatement("SELECT AVG(SCORE) FROM PRODUCT_COMMENT WHERE PRODUCT_ID = ?");
+					ps.setInt(1, list2.get(i).getProductid());
+					rSet = ps.executeQuery();
+
+					while (rSet.next()) {
+						DecimalFormat df = new DecimalFormat("#.#");
+						avg.add(Double.valueOf(df.format(rSet.getDouble(1))));
+					}
+					
+					
+					//以下找出所在城市
+					ps = connection
+							.prepareStatement("SELECT c.CITY\r\n"
+									+ "FROM PRODUCT p \r\n"
+									+ "	join PRODUCT_LOC pl\r\n"
+									+ "		on p.PRODUCT_ID = pl.PRODUCT_ID\r\n"
+									+ "         join City c \r\n"
+									+ "			on c.CITY_ID = pl.CITY_ID\r\n"
+									+ "where p.PRODUCT_ID = ?");
+					ps.setInt(1, list2.get(i).getProductid());
+					rSet = ps.executeQuery();
+
+					while (rSet.next()) {
+						cities.add(rSet.getString(1));
+					}
+					
+					
+					rSet.close();
+					ps.close();
 				}
 			}
 
+			
+			connection.close();
+			
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
-
-		
+		//顯示哪幾筆介紹
+		List<String> introStrings = new ArrayList<String>();
+		for(int i = 0 ; i < list2.size(); i++) {
+			String string = list2.get(i).getProductintro();
+//			String[] strs = string.split("<br>");
+//			String a = strs[0]+strs[1];
+//			String a = string.substring(0, 50);
+			String a = string.replaceAll("<br>", " ");
+			a = a.substring(0, 50);
+			
+			introStrings.add(a);
+		}
+			
+		model.addAttribute("cities", cities);
+		model.addAttribute("avg", avg);
+		model.addAttribute("commentcount", commentcount);
+		model.addAttribute("introStrings", introStrings);
 		model.addAttribute("keyword", session.getAttribute("keyword"));
 //		session.removeAttribute("keyword");
 		model.addAttribute("type", session.getAttribute("type"));
