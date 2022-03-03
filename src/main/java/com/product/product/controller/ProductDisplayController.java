6e20dcf9a7ee42fcf51bdf28614db5bbb2857f69
Gin <shuaka60@gmail.com>
2022-03-03 12:31:02 +0800

    3/3 state bug repaired

--- src/main/java/com/product/product/controller/ProductDisplayController.java
+++ src/main/java/com/product/product/controller/ProductDisplayController.java
@@ -125,9 +125,11 @@ public class ProductDisplayController {
 			}
 		}
 
+		//上架過濾
 		for (int i = 0; i < list.size(); i++) {
 			if (list.get(i).getState() == 0) {
 				list.remove(list.get(i));
+				i--;
 			}
 		}
 
